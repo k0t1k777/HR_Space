a20f900dee8f191d048f6bb3c575d77be4e22d26
Nickolai <lindeman072@mail.ru>
2024-03-13 23:59:16 +0400

    add: statusBar

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Title from '../Title/Title';
 import './StatusBar.css';
 
-// interface TitleProps {
-//   title: string;
+// interface StatusBarProps {
+//   index: any;
 //   onSave: (newTitle: string) => void;
 // }
 
@@ -13,23 +13,58 @@ export default function StatusBar() {
     setNewTitle(title);
   };
 
-  
+  const [selectedStatus, setSelectedStatus] = useState<number | null>(null);
+  const [clicked, setClicked] = useState(false);
 
-  
+  const changeColor = (index: any) => {
+    if (!clicked) {
+      setSelectedStatus(index);
+    }
+  };
+
+  const resetColor = () => {
+    if (!clicked) {
+      setSelectedStatus(null);
+    }
+  };
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLDivElement>,
+    index: number
+  ) => {
+    event.stopPropagation();
+    setClicked(true);
+    setSelectedStatus(null);
+    changeColor(index);
+  };
+
+  // useEffect(() => {
+  //   if (clicked) {
+  //     resetColor();
+  //     setClicked(false);
+  //   }
+  // }, [clicked]);
+
   return (
     <div className='status-bar'>
-        <Title title={newTitle} onSave={handleSaveTitle} />
+      <Title title={newTitle} onSave={handleSaveTitle} />
+
       <div className='status-bar__container-status'>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
-        <div className='statusBar__item'></div>
+        {[...Array(10).keys()].map((index) => (
+          <div
+            className='statusBar__item'
+            key={index}
+            style={{
+              backgroundColor:
+                selectedStatus !== null && index <= selectedStatus
+                  ? '#FF4D3A'
+                  : '',
+            }}
+            onMouseEnter={() => changeColor(index)}
+            onMouseLeave={resetColor}
+            onClick={(e) => handleClick(e, index)}
+          ></div>
+        ))}
       </div>
       <p className='status-bar__description'>Описание вакансии</p>
     </div>
