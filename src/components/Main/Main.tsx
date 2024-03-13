625bf807e68f88172ec3342d16fec5f8fd0a19c0
Kim Se-Un <kseun1949@gmail.com>
2024-03-13 22:40:22 +0300

    add: make up header, change place BrowserRouter

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import "./Main.css";
 import Sidebar from "../Main/Sidebar/Sidebar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// BrowserRouter moved in main.tsx
+import { Route, Routes } from "react-router-dom"; 
 import Сandidate from "../Main/Сandidate/Сandidate";
 import Сooperation from "../Main/Сooperation/Сooperation";
 import Description from "../Main/Description/Description";
@@ -9,7 +10,6 @@ import Requirement from "../Main/Requirement/Requirement";
 export default function Main() {
   return (
     <main className="main">
-    <BrowserRouter>
       <Sidebar />
       <Routes>
         <Route path="/description" element={<Description />} />
@@ -17,7 +17,6 @@ export default function Main() {
         <Route path="/conditions-for-candidate" element={<Сandidate />} />
         <Route path="/conditions-for-cooperation" element={<Сooperation />} />
       </Routes>
-    </BrowserRouter>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// moved here BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
