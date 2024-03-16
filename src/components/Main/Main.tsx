63a6176f320ea36c8c8bb8968bddf71a063a721e
Kim Se-Un <kseun1949@gmail.com>
2024-03-16 13:01:40 +0300

    fix: fix layout component BigPictureContainer

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,8 @@
 import "./Main.css";
-//import Sidebar from "../Main/Sidebar/Sidebar";
+//import Sidebar from "../Main/Sidebar/Sidebar"; delete SideBar
 // BrowserRouter moved in main.tsx
-import { Route, Routes } from "react-router-dom"; 
+import { Route, Routes } from "react-router-dom";
+import BigPictureContainer from "../BigPictureContainer/BigPictureContainer"; 
 import Сandidate from "../Main/Сandidate/Сandidate";
 import Сooperation from "../Main/Сooperation/Сooperation";
 import Description from "../Main/Description/Description";
@@ -10,6 +11,7 @@ import Requirement from "../Main/Requirement/Requirement";
 export default function Main() {
   return (
     <main className="main">
+      <BigPictureContainer />
       <Routes>
         <Route path="/description" element={<Description />} />
         <Route path="/equirement" element={<Requirement />} />
