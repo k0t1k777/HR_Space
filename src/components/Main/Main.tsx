46a3d23494642f7e5d1c57ac9768965c1c5fe2a4
Kim Se-Un <kseun1949@gmail.com>
2024-03-16 15:31:23 +0300

    add/ create branch add/slider

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,16 +2,16 @@ import "./Main.css";
 //import Sidebar from "../Main/Sidebar/Sidebar"; delete SideBar
 // BrowserRouter moved in main.tsx
 import { Route, Routes } from "react-router-dom";
-import BigPictureContainer from "../BigPictureContainer/BigPictureContainer"; 
+//import BigPictureContainer from "../BigPictureContainer/BigPictureContainer"; 
 import Сandidate from "../Main/Сandidate/Сandidate";
 import Сooperation from "../Main/Сooperation/Сooperation";
 import Description from "../Main/Description/Description";
 import Requirement from "../Main/Requirement/Requirement";
 
+
 export default function Main() {
   return (
     <main className="main">
-      <BigPictureContainer />
       <Routes>
         <Route path="/description" element={<Description />} />
         <Route path="/equirement" element={<Requirement />} />
