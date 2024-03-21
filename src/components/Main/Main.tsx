4f93a535c0d0d183441d61b3f486586f1b351338
Nickolai <lindeman072@mail.ru>
2024-03-21 23:49:02 +0400

    fix: after mearge tutn validation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -49,48 +49,46 @@ export default function Main() {
   const [isValid, setIsValid] = useState(true);
 
   const handleContinue = () => {
-    // if (currentStep === 1) {
-    //   if (
-    //     inputValueSpecialty.trim() !== '' &&
-    //     inputValueSalaryMin.trim() !== '' &&
-    //     inputValueSalaryMax.trim() !== ''
-    //   ) {
-    //     setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
-    //     setIsValid(true);
-    //   } else {
-    //     setIsValid(false);
-    //     console.error('Поля обязательны для заполнения');
-    //   }
-    // } else if (currentStep === 2) {
-    //   if (inputValuesDuties.trim() !== '') {
-    //     setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
-    //     setIsValid(true);
-    //   } else {
-    //     setIsValid(false);
-    //     console.error('Поле обязательно для заполнения');
-    //   }
-    //   } else if (currentStep === 3) {
-    //     if (added.length !== 0) {
-    //       setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
-    //       setIsValid(true);
-    //     } else {
-    //       setIsValid(false);
-    //       console.error('Поле обязательно для заполнения');
-    //     }
-
-    // } else if (currentStep === 4) {
-    //   if (inputValuesLanguage.trim() !== '') {
-    //     setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
-    //     setIsValid(true);
-    //   } else {
-    //     setIsValid(false);
-    //     console.error('Поле обязательно для заполнения');
-    //   }
-
-    // } else {
-    setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
-    setIsValid(true);
-    // }
+    if (currentStep === 1) {
+      if (
+        inputValueSpecialty.trim() !== '' &&
+        inputValueSalaryMin.trim() !== '' &&
+        inputValueSalaryMax.trim() !== ''
+      ) {
+        setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
+        setIsValid(true);
+      } else {
+        setIsValid(false);
+        console.error('Поля обязательны для заполнения');
+      }
+    } else if (currentStep === 2) {
+      if (inputValuesDuties.trim() !== '') {
+        setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
+        setIsValid(true);
+      } else {
+        setIsValid(false);
+        console.error('Поле обязательно для заполнения');
+      }
+    } else if (currentStep === 3) {
+      if (added.length !== 0) {
+        setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
+        setIsValid(true);
+      } else {
+        setIsValid(false);
+        console.error('Поле обязательно для заполнения');
+      }
+    } else if (currentStep === 4) {
+      if (inputValuesLanguage.trim() !== '') {
+        setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
+        setIsValid(true);
+      } else {
+        setIsValid(false);
+        console.error('Поле обязательно для заполнения');
+      }
+    } else {
+      setCurrentStep((prevStep) => (prevStep < 9 ? prevStep + 1 : prevStep));
+      setIsValid(true);
+    }
   };
 
   const handleBack = () => {
